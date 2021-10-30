234941147585698cf3297ca961ed3b48db13cac2
Brian Chief Obare <brian.obare@pfizer.com>
2021-10-30 09:56:10 +0300

    roles backend

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
@@ -18,4 +19,14 @@ class Role extends Model
         'name',
         'description',
     ];
+
+    /**
+     * Relationship with UserRoles Model
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function user(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
