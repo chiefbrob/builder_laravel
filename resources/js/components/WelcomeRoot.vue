9aa8fbaa8f632471f454b08d8e7474e91c03b404
Brian Chief Obare <brian.obare@pfizer.com>
2022-03-12 14:34:59 +0300

    basic welcome

--- resources/js/components/WelcomeRoot.vue
+++ resources/js/components/WelcomeRoot.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
     <nav-root></nav-root>
-    <h1>Welcome</h1>
+    <div class="row py-5">
+      <div class="col-md-3 offset-md-1">
+        <b-card
+          title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-2"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the bulk of the card's
+            content.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </div>
+      <div class="col-md-7">
+        <b-tabs content-class="mt-3" align="center">
+          <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+          <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+          <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+        </b-tabs>
+      </div>
+    </div>
   </div>
 </template>
