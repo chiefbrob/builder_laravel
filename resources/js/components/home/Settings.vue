6719890392589e89b8ed858984cb4d8af7f91ea2
Brian Chief Obare <brian.obare@pfizer.com>
2022-04-24 11:01:05 +0300

    fix

--- resources/js/components/home/Settings.vue
+++ resources/js/components/home/Settings.vue
@@ -6,13 +6,14 @@
       <b-card class="col-md-8 offset-md-2" title="Settings">
         <b-tabs pills card vertical nav-wrapper-class="w-50">
           <b-tab title="Your Account" active
-            ><b-card-text>
-              <account-settings></account-settings> </b-card-text
+            ><b-card-text> <account-settings></account-settings> </b-card-text
           ></b-tab>
-          <b-tab title="Security and account access"
+          <b-tab v-if="false" title="Security and account access"
             ><b-card-text>Security and account access</b-card-text></b-tab
           >
-          <b-tab title="Privacy and Safety"><b-card-text>Privacy and Safety</b-card-text></b-tab>
+          <b-tab disabled title="Privacy and Safety"
+            ><b-card-text>Privacy and Safety</b-card-text></b-tab
+          >
         </b-tabs>
       </b-card>
     </div>
