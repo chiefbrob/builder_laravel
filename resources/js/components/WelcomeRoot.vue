c0a738d5c12e578433a872ce82c904136a9732d1
Brian Chief Obare <brian.obare@pfizer.com>
2022-11-17 19:50:55 +0300

    update welcome message

--- resources/js/components/WelcomeRoot.vue
+++ resources/js/components/WelcomeRoot.vue
@@ -2,31 +2,28 @@
   <div>
     <nav-root></nav-root>
     <div class="row py-5 mb-5">
-      <div class="col-md-3 offset-md-1">
-        <b-card
-          title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          class="mb-2"
-        >
-          <b-card-text>
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </b-card-text>
+      <div
+        class="col-md-10 offset-md-1
+      "
+      >
+        <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
+          <template #header>{{ $root.$store.state.config.name }}</template>
 
-          <b-button href="#" variant="primary">Go somewhere </b-button>
+          <template #lead>
+            This is a simple hero unit, a simple jumbotron-style component for calling extra
+            attention to featured content or information.
+          </template>
 
-          <clipboard content="copy me"></clipboard>
-        </b-card>
-      </div>
-      <div class="col-md-7">
-        <b-tabs content-class="mt-3" align="center">
-          <b-tab title="First" active><p>I'm the first tab</p></b-tab>
-          <b-tab title="Second"><p>I'm the second tab</p></b-tab>
-          <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
-        </b-tabs>
+          <hr class="my-4" />
+
+          <p>
+            It uses utility classes for typography and spacing to space content out within the
+            larger container.
+          </p>
+          <p>
+            <clipboard content="copy me" variant="dark"></clipboard>
+          </p>
+        </b-jumbotron>
       </div>
     </div>
     <page-footer></page-footer>
