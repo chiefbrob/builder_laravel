0d58187e1a4b546fdf48fd8e525cbf10e1912018
Brian Chief Obare <brianobare@gmail.com>
2023-02-13 21:28:54 +0300

    Admin UI (#56)
    * centralize header and admin updates
    
    * update readme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-        <title>{{ config('app.name') }}</title>
-
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="{{ config('app.description') }}">
-        <meta name="theme-color" content="{{ config('app.theme_color') }}"/>
-        {{-- <meta http-equiv="Content-Security-Policy" content="default-src 'self'"> --}}
-        <meta name="application-name" content="{{ config('app.name') }}">
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-        <link rel="manifest" href="/manifest.json">
-        <link rel="apple-touch-icon" href="/images/180.png">
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
         @include('header-links')
 
