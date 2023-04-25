bac656458f80087296d00278e85ae1977b3b24f7
Brian Chief Obare <brianobare@gmail.com>
2023-04-25 17:32:02 +0300

    ui changes

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -4,8 +4,9 @@ import { shop } from './shop';
 import { auth } from './auth';
 import { system } from './system';
 import { roles } from './admin/roles';
+import { admin } from './admin/admin';
 
-const routes = [...roles, ...teams, ...blog, ...shop, ...auth, ...system];
+const routes = [...roles, ...teams, ...blog, ...shop, ...auth, ...admin, ...system];
 
 export default {
   mode: 'history',
