60ec7fc85da25b46d5e17297104b3884623ebf8f
Brian Chief Obare <brianobare@gmail.com>
2023-05-07 12:31:21 +0300

    switch login to username

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var string[]
      */
     protected $fillable = [
+        'username',
         'name',
         'email',
         'phone_number',
