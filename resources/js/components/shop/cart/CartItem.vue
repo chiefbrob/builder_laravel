f4b9efbe4476a966bf9b60008afdd51c01266a08
Brian Obare <brianobare@gmail.com>
2023-10-29 13:19:27 +0300

    customize cart item

--- resources/js/components/shop/cart/CartItem.vue
+++ resources/js/components/shop/cart/CartItem.vue
@@ -1,9 +1,10 @@
 <template>
   <b-card style="padding: 0;">
-    <b-card-text>
+    <b-card-text class="pointer" @click="showProduct" style="margin: 0; padding: 0">
       {{ item.product_variant.name.substr(0, 15)
       }}{{ item.product_variant.name.length > 15 ? '...' : '' }}
     </b-card-text>
+    <b-card-sub-title>{{ item.product.name }}</b-card-sub-title>
     <b-card-img
       :src="
         item.product_variant.photo
@@ -13,23 +14,26 @@
     ></b-card-img>
     <b-card-text class="pt-1">
       <p>
-        <b-button size="sm" variant="info" class="text-white">- </b-button>
+        <b-button v-if="item.quantity > 1" size="sm" variant="info" class="text-white py-0"
+          >-
+        </b-button>
         {{ item.quantity }}
         <b-button
           size="sm"
           variant="info"
-          class="text-white"
+          v-if="item.product_variant.quantity > 1"
+          class="text-white py-0"
           :disabled="
             item.quantity === item.product_variant.quantity || item.product_variant.quantity === 0
           "
           >+
         </b-button>
         <b-badge :variant="badgeVariant" class="text-white">{{ badgeText }}</b-badge>
-        <b-button class="float-right" variant="danger" size="sm"
+        <b-button class="float-right py-0" variant="danger" size="sm"
           ><i class="fa fa-trash-can"></i
         ></b-button></p
     ></b-card-text>
-    <b-card-footer>{{ item.product.name }} <br />{{ item.product.price | kes }}</b-card-footer>
+    <b-card-footer>{{ (item.product.price * item.quantity) | kes }}</b-card-footer>
   </b-card>
 </template>
 
@@ -42,7 +46,19 @@
     },
     computed: {
       badgeVariant() {
-        return 'info';
+        switch (this.item.product_variant.quantity) {
+          case 0:
+            return 'default';
+            break;
+          case 1:
+          case 2:
+          case 3:
+            return 'danger';
+
+          default:
+            return `info`;
+            break;
+        }
       },
       badgeText() {
         switch (this.item.product_variant.quantity) {
@@ -50,7 +66,7 @@
             return 'out of stock';
             break;
           case 1:
-            return 'last one';
+            return 'last one!';
           case 2:
           case 3:
             return 'less than 3 left';
@@ -60,14 +76,15 @@
             break;
         }
       },
-      lastOne() {
-        return this.item.product_variant.quantity === 1;
-      },
-      lastThree() {
-        return this.item.product_variant.quantity > 1 && this.item.product_variant.quantity <= 3;
-      },
-      outOfStock() {
-        return this.item.product_variant.quantity === 0;
+    },
+    methods: {
+      showProduct() {
+        this.$router.push({
+          name: 'view-product',
+          params: {
+            slug: this.item.product.slug,
+          },
+        });
       },
     },
   };
