4e9ca007feb27a495ea9faae341397e75d50a6df
Brian Obare <brianobare@gmail.com>
2023-10-31 20:35:54 +0300

    ui changes

--- resources/js/components/shop/ShopRoot.vue
+++ resources/js/components/shop/ShopRoot.vue
@@ -2,17 +2,6 @@
   <div>
     <div class="mb-5 pb-5 row">
       <div class="col-md-10 offset-md-1">
-        <h4 class="pt-1">
-          Shop
-          <b-button
-            v-if="admin"
-            variant="success"
-            @click="$router.push({ name: 'create-product' })"
-            size="sm"
-            class="float-right"
-            ><i class="fa fa-plus"></i> Add Item</b-button
-          >
-        </h4>
         <products-list></products-list>
       </div>
     </div>
