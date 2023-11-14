5b2b165cd163c7e61ad47a186356a9a53a94723e
Brian Obare <brianobare@gmail.com>
2023-11-15 02:31:08 +0300

    can get featured products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,6 +22,7 @@ class ProductFactory extends Factory
             'price' => $this->faker->numberBetween(1000, 3000),
             'description' => $this->faker->text,
             'photo' => null,
+            'featured' => $this->faker->boolean()
         ];
     }
 }
