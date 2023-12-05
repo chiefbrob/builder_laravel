52bf8433c06408baf7b6cb17da98ddcca9ee0e3d
Brian Obare <brianobare@gmail.com>
2023-12-06 02:34:30 +0300

    backup to google drive works

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace App\Providers;
 
+use Exception;
+use Google\Client;
+use Google\Service\Drive;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Masbug\Flysystem\GoogleDriveAdapter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +31,39 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadGoogleStorageDriver();
+    }
+
+    private function loadGoogleStorageDriver(string $driverName = 'google') 
+    {
+        try {
+            Storage::extend(
+                $driverName, 
+                function ($app, $config) {
+                    $options = [];
+        
+                    if (!empty($config['teamDriveId'] ?? null)) {
+                        $options['teamDriveId'] = $config['teamDriveId'];
+                    }
+        
+                    $client = new Client();
+                    $client->setClientId($config['clientId']);
+                    $client->setClientSecret($config['clientSecret']);
+                    $client->refreshToken($config['refreshToken']);
+        
+                    $service = new Drive($client);
+                    $adapter = new GoogleDriveAdapter(
+                        $service, 
+                        $config['folder'] ?? '/', 
+                        $options
+                    );
+                    $driver = new Filesystem($adapter);
+        
+                    return new FilesystemAdapter($driver, $adapter);
+                }
+            );
+        } catch(Exception $e) {
+            Log::error($e);
+        }
     }
 }
