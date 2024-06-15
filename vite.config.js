7b0ecf78b507a024ff79c2ed84e5ba66576ce2b8
Shift <shift@laravelshift.com>
2024-06-15 15:21:02 +0000

    Configure Vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue2';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/sass/main.scss',
+                'resources/js/app.js',
+                'resources/js/main.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
